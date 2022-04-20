88c3b29a4c4d4a2f99214a503d9c53f2bac88864
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-04-20 23:54:40 +0100

    Commit del método load

--- module/src/Cargo.java
+++ module/src/Cargo.java
@@ -14,4 +14,11 @@ public class Cargo extends SpaceShip{
     public static void setCapacidad(int capacidad) {
         Cargo.capacidad = capacidad;
     }
+    public static void load() {
+        if (capacidad >= MAX_CAPACIDAD) {
+            System.out.println("La nave está llena !!!");
+        }else {
+            capacidad ++;
+        }
+    }
 }
